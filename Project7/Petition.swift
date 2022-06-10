b9027fdfe58eccf1a643704611f932eac33b15a1
Mert Tekay <74730521+merttekay@users.noreply.github.com>
2022-06-10 11:28:06 +0300

    Project7

---
+++ Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Mert Tekay on 9.05.2022.
+//  Copyright © 2022 Mert Tekay. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    
+    var title:String
+    var body:String
+    var signatureCount:Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Mert Tekay on 9.05.2022.
+//  Copyright © 2022 Mert Tekay. All rights reserved.
+//
+
+import Foundation
+struct Petitions:Codable {
+    var results:[Petition]
+}
